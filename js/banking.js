39170bb64eb882e5e4bcac3b499edfb5f492e37b
shahadat2772 <shahadat2772@gmailcom>
2022-02-11 11:57:23 +0600

    depo and widraw field functional

--- js/banking.js
+++ js/banking.js
@@ -8,18 +8,15 @@ function getInputValue(inputId) {
   return amountValue;
 }
 
-// FUCTION TO GET THE TOTAL DEPOSIT
-function getTolatTransAmount(id) {
-  var totalDipo = document.getElementById(id).innerText;
-  var totalDipolInNumber = parseFloat(totalDipo);
-  return totalDipolInNumber;
-}
+// FUNCTION UPDATE TOTAL FIELD
 
-// FUNCTION TO GET THE LAST TOTAL BALANCE
-function getThelastTotalBalanceAmount(id) {
-  var totalBalance = document.getElementById(id).innerText;
-  var totalBalanceInNumber = parseFloat(totalBalance);
-  return totalBalanceInNumber;
+function updateTotalField(inputForTransInNumber, fieldId) {
+  var lastTotalTrans = document.getElementById(fieldId).innerText;
+  var lastTotalTransInNumber = parseFloat(lastTotalTrans);
+
+  // SETTING THE NEW TOTAL FIELD VALUE
+  document.getElementById(fieldId).innerText =
+    lastTotalTransInNumber + inputForTransInNumber;
 }
 
 // EVENT LISTENER FOR DEPOSIT BUTTON
@@ -28,19 +25,19 @@ document.getElementById("depositButton").addEventListener("click", function () {
   // var inputForDeposit = document.getElementById("depositInput").value;
   // var inputForDepositInNumber = parseFloat(inputForDeposit);
   var inputForDepositInNumber = getInputValue("depositInput");
-  // GETTING THE LAST DEPO VALUE
+  // GETTING THE LAST DEPO VALUE AND UPDATE IT
   //   var lastDipoTotal = document.getElementById("depositTotal").innerText;
   //   var lastDipoTotalInNumber = parseFloat(lastDipoTotal);
-  var lastDipoTotalInNumber = getTolatTransAmount("depositTotal");
 
-  // SETTING THE NEW TOTAL DEPO VALUE
-  document.getElementById("depositTotal").innerText =
-    inputForDepositInNumber + lastDipoTotalInNumber;
+  //   // SETTING THE NEW TOTAL DEPO VALUE
+  //   document.getElementById("depositTotal").innerText =
+  //     inputForDepositInNumber + lastDipoTotalInNumber;
+  updateTotalField(inputForDepositInNumber, "depositTotal");
+
   // GETTING THE LAST TOTAL BALANCE VALUE
-  //   var totalBalance = document.getElementById("totalBalance").innerText;
-  //   var totalBalanceInNumber = parseFloat(totalBalance);
-  var totalBalanceInNumber = getThelastTotalBalanceAmount("totalBalance");
-  // SETTING THE NEW TOTAL VALUE
+  var totalBalance = document.getElementById("totalBalance").innerText;
+  var totalBalanceInNumber = parseFloat(totalBalance);
+  // UPDATING THE NEW TOTAL VALUE
   document.getElementById("totalBalance").innerText =
     totalBalanceInNumber + inputForDepositInNumber;
 
@@ -54,18 +51,19 @@ document.getElementById("widrawButton").addEventListener("click", function () {
   //   var inputForWidraw = document.getElementById("widrawInput").value;
   //   var inputForWidrawInNumber = parseFloat(inputForWidraw);
   var inputForWidrawInNumber = getInputValue("widrawInput");
-  // GETTING THE LAST WID VALUE
+  // GETTING THE LAST WID VALUE AND UPDATE IT
   //   var lastWidTotal = document.getElementById("totalWidraw").innerText;
   //   var lastWidTotalInNumber = parseFloat(lastWidTotal);
-  var lastWidTotalInNumber = getTolatTransAmount("totalWidraw");
-  // SETTING THE NEW TOTAL WID VALUE
-  document.getElementById("totalWidraw").innerText =
-    inputForWidrawInNumber + lastWidTotalInNumber;
+
+  //   // SETTING THE NEW TOTAL WID VALUE
+  //   document.getElementById("totalWidraw").innerText =
+  //     inputForWidrawInNumber + lastWidTotalInNumber;
+  updateTotalField(inputForWidrawInNumber, "totalWidraw");
 
   // GETTING THE LAST TOTAL BALANCE VALUE
-  //   var totalBalance = document.getElementById("totalBalance").innerText;
-  //   var totalBalanceInNumber = parseFloat(totalBalance);
-  var totalBalanceInNumber = getThelastTotalBalanceAmount("totalBalance");
+  var totalBalance = document.getElementById("totalBalance").innerText;
+  var totalBalanceInNumber = parseFloat(totalBalance);
+
   // SETTING THE NEW TOTAL VALUE
   document.getElementById("totalBalance").innerText =
     totalBalanceInNumber - inputForWidrawInNumber;
